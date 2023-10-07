1318eceb31956e319655bc8618704d38d9348bcc
neuronadine <126725763+neuronadine@users.noreply.github.com>
2023-10-07 15:06:42 -0400

    Drawing experiment : added color to my cone

--- experiment/workflow/js/script.js
+++ experiment/workflow/js/script.js
@@ -22,13 +22,24 @@ function preload() {
 function setup() {
     createCanvas(500, 500);
 
-    background(255, 138, 1);
+    background(191, 255, 199);
 
     ellipseMode(CORNER);
+    noStroke();
+    
+    fill(127, 0, 200, 100);
     ellipse(250, 250, 100, 100);
+
+    fill(137, 0, 210, 100);
     ellipse(250, 250, 80, 80);
+
+    fill(147, 0, 220, 100);
     ellipse(250, 250, 60, 60);
+
+    fill(157, 0, 230, 100);
     ellipse(250, 250, 40, 40);
+
+    fill(167, 0, 240, 100);
     ellipse(250, 250, 20, 20);
 }
 
