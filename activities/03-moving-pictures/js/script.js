50d19ef455cd3e5328b46033a598c30f5cd1030f
neuronadine <126725763+neuronadine@users.noreply.github.com>
2023-10-08 15:20:06 -0400

    A3 : Completed the activity

--- activities/03-moving-pictures/js/script.js
+++ activities/03-moving-pictures/js/script.js
@@ -6,21 +6,38 @@
  * and this description to match your project!
  */
 
-"use strict";
+let bg = {
+    r : 0,
+    g : 0,
+    b : 0
+};
 
-/**
- * Description of preload
-*/
-function preload() {
+let circle1 = {
+    x : 0,
+    y : 250,
+    size : 100,
+    growthRate : 1,
+    speed : 1,
+    fill : 255,
+    alpha : 200 
+};
 
-}
+let circle2 = {
+    x : 500,
+    y : 250,
+    size : 75,
+    sizeRatio : 0.75,
+    speed : -1,
+    fill : 255,
+    alpha : 200 
+};
 
-
-/**
- * Description of setup
-*/
+// setup()
+//
+//
 function setup() {
-
+    createCanvas(500, 500);
+    noStroke();
 }
 
 
@@ -28,5 +45,22 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    // Background
+    background(bg.r, bg.g, bg.b);
+    bg.r = map(circle1.size, 100, width, 0, 255);
 
+    //Left circle
+    circle1.x += circle1.speed;
+    circle1.x = constrain(circle1.x, 0, width/2);
+    circle1.size += circle1.growthRate;
+    circle1.size = constrain(circle1.size, 0, width);
+    fill(circle1.fill, circle1.alpha);
+    ellipse(circle1.x, circle1.y, circle1.size);
+
+    //Right circle
+    circle2.x += circle2.speed;
+    circle2.x = constrain(circle2.x, width/2, width);
+    circle2.size = circle1.size * circle2.sizeRatio;
+    fill(circle2.fill, circle2.alpha);
+    ellipse(circle2.x, circle2.y, circle2.size);
 }
