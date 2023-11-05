f6c3d9d12e759a837fd0c59bbb3fe5d1b611ab0b
neuronadine <126725763+neuronadine@users.noreply.github.com>
2023-11-05 09:42:29 -0500

    Finished activity

--- activities/juggle_garden/js/script.js
+++ activities/juggle_garden/js/script.js
@@ -7,8 +7,10 @@
  */
 
 "use strict";
-
+let gravityForce = 0.002;
 let paddle;
+let balls = [];
+let numBalls = 3;
 
 function preload() {
 
@@ -18,6 +20,13 @@ function setup() {
 
     createCanvas(windowWidth, windowHeight);
     paddle = new Paddle(300, 20);
+
+    for (let i = 0; i < numBalls; i++) {
+        let x = random(0, width);
+        let y = random(-400, -100);
+        let ball = new Ball(x, y);
+        balls.push(ball);
+    }
 }
 
 
@@ -26,4 +35,16 @@ function draw() {
     
     paddle.move();
     paddle.display();
+
+    for ( let i = 0; i < balls.length; i++) {
+        let ball = balls[i];
+
+        if (ball.active) {
+            ball.gravity(gravityForce);
+            ball.move();
+            ball.bounce(paddle);
+            ball.display();
+        }
+        
+    }
 }
